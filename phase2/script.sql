b31ff51bd966920f4cab1dc2481484d597c6736f
Sentinent <Steaboo@gmail.com>
2021-05-19 18:47:20 -0700

    Changed interval to tsrange, added comments, changed `has` and `schedules` relations into fields

--- phase2/script.sql
+++ phase2/script.sql
@@ -1,104 +1,132 @@
-DROP TABLE Doctor CASCADE;  
-DROP TABLE Staff CASCADE;
-DROP TABLE request_maintenance CASCADE;
-DROP TABLE Department CASCADE; 
-DROP TABLE Hospital CASCADE;
-DROP TABLE Patient CASCADE;
-DROP TABLE searches CASCADE; /*weak entity?*/
-DROP TABLE Appointment CASCADE;
-DROP TABLE search_appnt CASCADE; /*optional*/
-DROP TABLE has CASCADE;
-DROP TABLE schedules CASCADE;
-DROP TABLE past CASCADE;
-DROP TABLE active CASCADE;
-DROP TABLE available CASCADE;
-DROP TABLE waitlisted CASCADE;
+/* Entities */
+DROP TABLE IF EXISTS Appointment CASCADE;
+DROP TABLE IF EXISTS Past_Appointment CASCADE;
+DROP TABLE IF EXISTS Active_Appointment CASCADE;
+DROP TABLE IF EXISTS Available_Appointment CASCADE;
+DROP TABLE IF EXISTS Waitlisted_Appointment CASCADE;
+
+DROP TABLE IF EXISTS Department CASCADE; 
+DROP TABLE IF EXISTS Doctor CASCADE;  
+DROP TABLE IF EXISTS Hospital CASCADE;
+DROP TABLE IF EXISTS Patient CASCADE;
+DROP TABLE IF EXISTS Staff CASCADE;
+
+/* Relationships */
+DROP TABLE IF EXISTS request_maintenance CASCADE;
+DROP TABLE IF EXISTS searches CASCADE;
+
+/* Entities */
+CREATE TABLE Hospital(
+	hospital_ID INTEGER NOT NULL,
+	name VARCHAR,
+
+	PRIMARY KEY(hospital_ID)
+);
+
+CREATE TABLE Department(
+	dept_ID INTEGER NOT NULL,
+	name VARCHAR,
+
+	/* foreign keys */
+	hospital_ID INTEGER NOT NULL, /* includes */
+	
+	PRIMARY KEY(dept_ID),
+	FOREIGN KEY(hospital_ID) REFERENCES Hospital
+);
 
 CREATE TABLE Doctor(
 	doctor_ID INTEGER NOT NULL,
-	name CHAR(30),
-	specialty CHAR(25),
-	dept_ID INTEGER NOT NULL,
-	PRIMARY KEY (doctor_ID),
-	FOREIGN KEY (dept_ID) REFERENCES Department
-);
-CREATE TABLE Staff(
-	staff_ID INTEGER NOT NULL,
-	name CHAR(30),
-	hospital_ID INTEGER NOT NULL,
-	PRIMARY KEY (staff_ID),
-	FOREIGN KEY (hospital_ID) REFERENCES Hospital
-);
-CREATE TABLE request_maintenance(
-	doctor_ID INTEGER NOT NULL,
-	staff_ID INTEGER NOT NULL,
-	patient_per_hour INTEGER,
-	dept_name CHAR(30),
-	time_slot INTERVAL(HOUR_TO_MINUTE),
-	PRIMARY KEY (doctor_ID, staff_ID),
-	FOREIGN KEY (doctor_ID) REFERENCES Doctor,
-	FOREIGN KEY (staff_ID) REFERENCES Staff
-);
-CREATE TABLE Department(
-	dept_ID INTEGER NOT NULL,
-	name CHAR(30),
-	hospital_ID INTEGER NOT NULL,
-	PRIMARY KEY (dept_ID),
-	FOREIGN KEY (hospital_ID) REFERENCES Hospital
-);
-CREATE TABLE Hospital(
-	hospital_ID INTEGER NOT NULL,
-	name CHAR(30),
-	PRIMARY KEY (hospital_ID) 	/*TO-DO: search_appnt??*/
-);
-CREATE TABLE Patient(
-	patient_ID INTEGER NOT NULL,
-	name CHAR(30),
-	gender CHAR(1),
-	age INTEGER,
-	address CHAR(40),
-	num_appnts INTEGER 	/*TO-DO: search_appnt??*/
+	name VARCHAR,
+	specialty VARCHAR,
+
+	/* foreign keys */
+	dept_ID INTEGER NOT NULL, /* works_dept */
+
+	PRIMARY KEY(doctor_ID),
+	FOREIGN KEY(dept_ID) REFERENCES Department
 );
 
-/*TO-DO: CREATE searches TABLE*/
+CREATE TABLE Staff(
+	staff_ID INTEGER NOT NULL,
+	name VARCHAR,
+
+	/* foreign keys */
+	hospital_ID INTEGER NOT NULL, /* works_in */
+
+	PRIMARY KEY(staff_ID),
+	FOREIGN KEY(hospital_ID) REFERENCES Hospital
+);
+
+CREATE TABLE Patient(
+	patient_ID INTEGER NOT NULL,
+	name VARCHAR,
+	gender VARCHAR,
+	age INTEGER,
+	address VARCHAR,
+	num_appnts INTEGER,
+	
+	PRIMARY KEY(patient_ID)
+);
 
 CREATE TABLE Appointment(
 	appnt_ID INTEGER NOT NULL,
 	date DATE,
-	time_slot INTERVAL(HOUR_TO_MINUTE), 	/*TO-DO: is_a attributes and search_appnt*/
-	PRIMARY KEY (appnt_ID)
+	time_slot TSRANGE,
+	type VARCHAR NOT NULL, /* used to choose the table from past, active, available, waitlisted */
+
+	/* foreign keys */
+	doctor_ID INTEGER NOT NULL, /* has */
+	staff_ID INTEGER NOT NULL, /* schedules */
+
+	PRIMARY KEY(appnt_ID),
+	FOREIGN KEY(doctor_ID) REFERENCES Doctor,
+	FOREIGN KEY(staff_ID) REFERENCES Staff
 );
-CREATE TABLE has(
+
+CREATE TABLE Past_Appointment(
+	appnt_ID INTEGER NOT NULL,
+	PRIMARY KEY(appnt_ID),
+	FOREIGN KEY(appnt_ID) REFERENCES Appointment
+);
+CREATE TABLE Active_Appointment(
+	appnt_ID INTEGER NOT NULL,
+	PRIMARY KEY(appnt_ID),
+	FOREIGN KEY(appnt_ID) REFERENCES Appointment
+);
+CREATE TABLE Available_Appointment(
+	appnt_ID INTEGER NOT NULL,
+	PRIMARY KEY(appnt_ID),
+	FOREIGN KEY(appnt_ID) REFERENCES Appointment
+);
+CREATE TABLE Waitlisted_Appointment(
+	appnt_ID INTEGER NOT NULL,
+	PRIMARY KEY(appnt_ID),
+	FOREIGN KEY(appnt_ID) REFERENCES Appointment
+);
+
+/* relationships */
+CREATE TABLE request_maintenance(
+	patient_per_hour INTEGER,
+	dept_name VARCHAR,
+	time_slot TSRANGE,
+
+	/* foreign keys */
 	doctor_ID INTEGER NOT NULL,
-	appnt_ID INTEGER NOT NULL,
-	PRIMARY KEY (doctor_ID, appnt_ID),
-	FOREIGN KEY (doctor_ID) REFERENCES Doctor,
-	FOREIGN KEY (appnt_ID) REFERENCES Appointment
-);
-CREATE TABLE schedules(
 	staff_ID INTEGER NOT NULL,
-	appnt_ID INTEGER NOT NULL,
-	PRIMARY KEY (staff_ID, appnt_ID),
-	FOREIGN KEY (staff_ID) REFERENCES Staff,
-	FOREIGN KEY (appnt_ID) REFERENCES Appointment
+
+	PRIMARY KEY(doctor_ID, staff_ID),
+	FOREIGN KEY(doctor_ID) REFERENCES Doctor,
+	FOREIGN KEY(staff_ID) REFERENCES Staff
 );
-CREATE TABLE past(
+
+CREATE TABLE searches(
+	/* foreign keys */
+	hospital_ID INTEGER NOT NULL,
+	patient_ID INTEGER NOT NULL,
 	appnt_ID INTEGER NOT NULL,
-	PRIMARY KEY (appnt_ID),
-	FOREIGN KEY (appnt_ID) REFERENCES Appointment
-);
-CREATE TABLE active(
-	appnt_ID INTEGER NOT NULL,
-	PRIMARY KEY (appnt_ID),
-	FOREIGN KEY (appnt_ID) REFERENCES Appointment
-);
-CREATE TABLE available(
-	appnt_ID INTEGER NOT NULL,
-	PRIMARY KEY (appnt_ID),
-	FOREIGN KEY (appnt_ID) REFERENCES Appointment
-);
-CREATE TABLE waitlisted(
-	appnt_ID INTEGER NOT NULL,
-	PRIMARY KEY (appnt_ID),
-	FOREIGN KEY (appnt_ID) REFERENCES Appointment
-);
+
+	PRIMARY KEY(hospital_ID, patient_ID, appnt_ID),
+	FOREIGN KEY(hospital_ID) REFERENCES Hospital,
+	FOREIGN KEY(patient_ID) REFERENCES Patient,
+	FOREIGN KEY(appnt_ID) REFERENCES Appointment
+)
